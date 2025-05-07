99204e6fd704a22b1e51b3d7de775a3dd1625b6c
azam <ig.azam069@gmail.com>
2025-05-07 10:20:03 +0530

    fixed conditional hook issue

--- app/edit/[id]/page.tsx
+++ app/edit/[id]/page.tsx
@@ -1,37 +1,32 @@
 'use client';
 
-import React,{useState} from "react";
+import React, { useState } from "react";
 import { useData } from "@/app/_component/DataProvider";
 
+const Edit = ({ params }: { params: { id: number } }) => {
+    const { id } = params; // remove Promise from params, Netlify can't handle that
+    const { state, dispatch } = useData();
 
-const Edit=({params}:{params:Promise<{id:number}>})=>{
-    const{id}=React.use(params);
-    const{state,dispatch}=useData();
-   
-    if(state.students[id])
-    {
-        console.log(state.students[id]);
+    const student = state.students[id];
+
+    const [data, setData] = useState(student || { name: "", age: 0, email: "" });
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setData({ ...data, [e.target.name]: e.target.value });
     }
-    else 
-    {
+
+    const handleUpdate = () => {
+        dispatch({ type: 'update', payload: { index: id, data: data } });
+        console.log("Data has been updated");
+    }
+
+    if (!student) {
         return <p>No record for edit</p>;
     }
-    const[data,setData]=useState(state.students[id]);
 
-    const handleChange=(e:React.ChangeEvent<HTMLInputElement>)=>{
-       setData({...data,[e.target.name]:e.target.value});
-    }
-   
-   const handleUpdate=()=>{
-       dispatch({type:'update',payload:{index:id,data:data}});
-       //setData({name:"",age:0,email:""});      
-       console.log("Data has been updated");
-   }
-
-
-    return(
+    return (
         <>
-            <h2>Edit Record of having id={id}</h2>
+            <h2>Edit Record of having id = {id}</h2>
             Name : <input type="text" name="name" value={data.name} onChange={handleChange} /><br />
             Age : <input type="number" name="age" value={data.age} onChange={handleChange} /><br />
             Email : <input type="email" name="email" value={data.email} onChange={handleChange} /><br />
@@ -39,4 +34,5 @@ const Edit=({params}:{params:Promise<{id:number}>})=>{
         </>
     );
 }
+
 export default Edit;
